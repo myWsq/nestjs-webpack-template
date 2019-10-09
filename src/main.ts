7a569d43f0652e0c2eddabd7b4f83152fa71c731
wangshuaiqi1 <wangshuaiqi1@xiaomi.com>
2019-10-09 17:30:09 +0800

    feat: 提示运行地址

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  // tslint:disable-next-line: no-console
+  console.log('Server is running on http://localhost:3000');
 
   if (module.hot) {
     module.hot.accept();
